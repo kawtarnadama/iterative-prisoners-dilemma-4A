19fc00c901cf586b56e4bd2ec5bb85a3a7e83c22
kawtarnadama <58440729+kawtarnadama@users.noreply.github.com>
2019-12-06 12:41:25 -0500

    Add files via upload

--- team3.py
+++ team3.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,9 +7,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'no u' # Only 10 chars displayed.
+strategy_name = 'what are the odds'
+strategy_description = 'decides based on what the opposing player favors'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,18 +18,26 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    Nb = 0
+    Nc = 0
+    choice = ['b','c']
+    for x in their_history:
+        if x == 'b':
+            Nb += 1
+        else:
+            Nc += 1
+    if len(my_history) <= 5:
+        return 'b'
+    elif len(my_history) > 5 and len(my_history) <= 10:
+        return 'c'
+    else:
+        if Nb > Nc:
+            return random.choice(choice)
+        elif Nb < Nc:
+            return 'b'
+        else:
+            return random.choice(choice)
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
@@ -53,7 +62,7 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print 'Test passed'
+         print('Test passed')
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
